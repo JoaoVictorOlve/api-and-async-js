abe23c5ed1b7eb60c0e5b93bbc35afdd65ddb479
João Victor <jvoliveira2005@gmail.com>
2023-05-11 21:12:13 -0300

    Curso de fetch api
    Parei no minuto 27:06, corrigindo bug referente ID e postID

--- fetch-api/script.js
+++ fetch-api/script.js
@@ -3,6 +3,18 @@ const url = "https://jsonplaceholder.typicode.com/posts"
 const loadingElement = document.querySelector("#loading")
 const postsContainer = document.querySelector("#posts-container")
 
+const postPage = document.querySelector("#post");
+const postContainer = document.querySelector("#post-container");
+const commentsContainer = document.querySelector("#comments-container");
+
+//Get id from URL
+
+const urlSearchParams = new URLSearchParams(window.location.search);
+const postId = urlSearchParams.get("id");
+
+console.log(postId)
+
+
 // Get all posts
 
 async function getAllPosts(){
@@ -27,7 +39,32 @@ async function getAllPosts(){
         link.innerText = "Ler"
         link.setAttribute("href", `/post.html?id=${post.id}`)
 
+        div.appendChild(title);
+        div.appendChild(body);
+        div.appendChild(link);
+
+        postsContainer.appendChild(div);
     })
 }
 
-getAllPosts()
+async function getPost(id){
+
+    const [responsePost, responseComments] = await Promise.all([
+        fetch(`${url}/${id}`),
+        fetch(`${url}/${id}/comments`)
+    ])
+
+    const dataPost = await responsePost.json();
+
+    const dataComments = await responseComments.json();
+
+    loadingElement.classList.add("hide");
+    postPage.classList.remove("hide");
+
+}
+
+if(!postId){
+    getAllPosts()
+} else {
+    getPost(postId);
+}
