6a712ba33b791b05b10746bce9a15c3e5b1bbbd0
João Victor <jvoliveira2005@gmail.com>
2023-05-12 21:41:21 -0300

    Finalizando tutorial fetch api

--- fetch-api/script.js
+++ fetch-api/script.js
@@ -7,12 +7,14 @@ const postPage = document.querySelector("#post");
 const postContainer = document.querySelector("#post-container");
 const commentsContainer = document.querySelector("#comments-container");
 
+const commentForm = document.querySelector("#comment-form")
+const emailInput = document.querySelector("#email")
+const bodyInput = document.querySelector("#body")
+
 //Get id from URL
 
 const urlSearchParams = new URLSearchParams(window.location.search);
 const postId = urlSearchParams.get("id");
-
-console.log(postId)
 
 
 // Get all posts
@@ -47,24 +49,81 @@ async function getAllPosts(){
     })
 }
 
+// Get individual post
 async function getPost(id){
 
-    const [responsePost, responseComments] = await Promise.all([
-        fetch(`${url}/${id}`),
-        fetch(`${url}/${id}/comments`)
-    ])
+  const [responsePost, responseComments] = await Promise.all([
+    fetch(`${url}/${id}`),
+    fetch(`${url}/${id}/comments`),
+  ]);
 
-    const dataPost = await responsePost.json();
+  const dataPost = await responsePost.json();
 
-    const dataComments = await responseComments.json();
+  const dataComments = await responseComments.json();
 
-    loadingElement.classList.add("hide");
-    postPage.classList.remove("hide");
+  loadingElement.classList.add("hide");
+  postPage.classList.remove("hide");
 
+  const title = document.createElement("h1");
+  const body = document.createElement("p");
+
+  title.innerText = dataPost.title;
+  body.innerText = dataPost.body;
+
+  postContainer.appendChild(title);
+  postContainer.appendChild(body);
+
+  dataComments.map((comment) => {
+    createComment(comment);
+  });
+}
+
+function createComment(comment){
+
+    const div = document.createElement("div")
+    const email = document.createElement("h3")
+    const commentBody = document.createElement("p")
+
+    email.innerText = comment.email
+    commentBody.innerText = comment.body
+
+    div.appendChild(email)
+    div.appendChild(commentBody)
+
+    commentsContainer.appendChild(div)
+}
+
+async function postComment(comment){
+
+    const response = await fetch(url, {
+        method: "POST",
+        body: comment,
+        headers: {
+            "Content-type":"application/json",
+        },
+    });
+
+    const data = await response.json();
+
+    createComment(data);
 }
 
 if(!postId){
     getAllPosts()
-} else {
-    getPost(postId);
+} else{
+    getPost(postId)
+
+    //Add event to comment form
+    commentForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+    
+        let comment = {
+          email: emailInput.value,
+          body: bodyInput.value,
+        };
+
+        comment = JSON.stringify(comment)
+
+        postComment(comment)
+      });
 }
